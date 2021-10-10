7acef66c41f69f2b68435e28eec2edcc7b7240e6
daoloc93 <daoloc93@gmail.com>
2021-10-10 15:52:08 +0700

    Change sqlite to Postgre

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ import os
 
 app = Flask(__name__)
 # OWN_SECRET_KEY = os.environ("SECRET_KEY")
-app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
